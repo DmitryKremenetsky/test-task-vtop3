fa5801fe4288de359c06605e752da4245d71aea5
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-08-31 16:55:35 +0300

    fix main

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const fishElements = document.querySelectorAll(".banner-item");
-
-  fishElements.forEach(function (fish) {
-    const randomAmplitude = Math.floor(Math.random() * 30) + 10;
-    fish.style.animation = `fish-swim 2s alternate infinite ${randomAmplitude}ms`;
-  });
-});
