5892ee21b6ee3f2e85832491b56d9f353898826b
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-09-01 16:17:06 +0300

    fix scroll bug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const languageToggle = document.getElementById("language-toggle");
 const burgerToggle = document.querySelector(".burger-menu");
 const burgerMenu = document.querySelector(".menu");
 const menuOverFlow = document.querySelector(".screen-noscroll");
+const burgerToggleActive = document.querySelector("burger-menu.active");
 
 let currentLanguage = "en";
 
@@ -21,7 +22,13 @@ languageToggle.addEventListener("click", function () {
 languageToggle.setAttribute("data-lang", currentLanguage);
 
 burgerToggle.addEventListener("click", () => {
-  burgerToggle.classList.toggle("active");
-  burgerMenu.classList.toggle("active");
-  menuOverFlow.classList.toggle("active");
+  if (burgerToggle.classList.contains("active")) {
+    burgerToggle.classList.remove("active");
+    burgerMenu.classList.remove("active");
+    menuOverFlow.classList.remove("active");
+  } else {
+    burgerToggle.classList.add("active");
+    burgerMenu.classList.add("active");
+    menuOverFlow.classList.add("active");
+  }
 });
