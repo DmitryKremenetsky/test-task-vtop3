ea2ba84ae2e34f6824b268f95d77791bc14e2860
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-09-01 16:00:33 +0300

    add mobile view

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const languageToggle = document.getElementById("language-toggle");
+const burgerToggle = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".menu");
+const menuOverFlow = document.querySelector(".screen-noscroll");
 
 let currentLanguage = "en";
 
@@ -16,3 +19,9 @@ languageToggle.addEventListener("click", function () {
 });
 
 languageToggle.setAttribute("data-lang", currentLanguage);
+
+burgerToggle.addEventListener("click", () => {
+  burgerToggle.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
+  menuOverFlow.classList.toggle("active");
+});
